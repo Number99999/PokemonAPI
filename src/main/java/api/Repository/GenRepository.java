605d804b1e53c9577c4d31ae5156f547145cbc69
Number99999 <90398616+Number99999@users.noreply.github.com>
2022-08-17 11:52:24 +0700

    update

--- src/main/java/api/Repository/GenRepository.java
+++
@@ -1,7 +0,0 @@
-package api.Repository;
-
-import api.Model.Gen;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenRepository extends JpaRepository<Gen, String> {
-}
--- src/main/java/api/Repository/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package api.Repository;
-
-import api.Model.Pokemon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PokemonRepository extends JpaRepository<Pokemon, String> {
-}
